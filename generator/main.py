7a8a5ecd07e5540e11487dd0d3b8f5858a42ec90
AlexM <dawenm@gmail.com>
2026-08-05 13:32:03 -0400

    feat(generator): add reusable utility functions

--- generator/main.py
+++ generator/main.py
@@ -1,77 +1,25 @@
-# from generator.config import Config
-# from generator.logger import get_logger
-
-# logger = get_logger(__name__)
-
-
-# def main():
-
-#     Config.create_directories()
-#     # print(Config.LOGS)
-#     logger.info("=" * 70)
-#     logger.info("Enterprise SAS → Python Migration")
-#     logger.info("=" * 70)
-
-#     logger.info("Project Root : %s", Config.ROOT)
-#     logger.info("Raw Data     : %s", Config.RAW_DATA)
-#     logger.info("Processed    : %s", Config.PROCESSED_DATA)
-#     logger.info("Output       : %s", Config.OUTPUT)
-
-#     logger.info("Application started successfully.")
-
-
-# if __name__ == "__main__":
-#     main()
-
-from datetime import datetime
-
+from generator.config import Config
 from generator.logger import get_logger
-from generator.utils import (
-    random_date,
-    random_gender,
-    random_phone,
-    random_email,
-    random_zip,
-)
 
 logger = get_logger(__name__)
 
 
 def main():
 
-    logger.info("Testing utility functions")
+    Config.create_directories()
+    # print(Config.LOGS)
+    logger.info("=" * 70)
+    logger.info("Enterprise SAS → Python Migration")
+    logger.info("=" * 70)
 
-    logger.info(
-        "Random DOB: %s",
-        random_date(
-            datetime(1950, 1, 1),
-            datetime(2020, 12, 31)
-        )
-    )
+    logger.info("Project Root : %s", Config.ROOT)
+    logger.info("Raw Data     : %s", Config.RAW_DATA)
+    logger.info("Processed    : %s", Config.PROCESSED_DATA)
+    logger.info("Output       : %s", Config.OUTPUT)
 
-    logger.info(
-        "Gender: %s",
-        random_gender()
-    )
-
-    logger.info(
-        "Phone: %s",
-        random_phone()
-    )
-
-    logger.info(
-        "Email: %s",
-        random_email(
-            "John",
-            "Smith"
-        )
-    )
-
-    logger.info(
-        "ZIP: %s",
-        random_zip()
-    )
+    logger.info("Application started successfully.")
 
 
 if __name__ == "__main__":
     main()
+
